e9688772370071c195d6359330a9e88a1f2a37b0
Rajashekar Chintalapati <rchintalapati@walmartlabs.com>
2019-11-02 22:33:15 -0700

    First commit

---
+++ grades.py
@@ -0,0 +1,10 @@
+names = input("Enter names seperated by commas:").split(",") # get and process input for a list of names
+assignments = input("Enter assignment counts seperated by commas: ").split(",")# get and process input for a list of the number of assignments
+grades = input("Enter grades seperated by commas: ").split(",")# get and process input for a list of grades
+
+message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
+submit before you can graduate. You're current grade is {} and can increase \
+to {} if you submit all assignments before the due date.\n\n"
+
+for name, assignment, grade in zip(names, assignments, grades):
+    print(message.format(name, assignment, grade, int(grade) + 2 * int(assignment)))
